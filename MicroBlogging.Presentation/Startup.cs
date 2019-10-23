7cdbce2beccb44845b3eb21a40e61e97b29c9aa6
Azgarion <arthur.td@hotmail.fr>
2019-10-23 16:05:38 +0200

    First connection to database

--- MicroBlogging.Presentation/Startup.cs
+++ MicroBlogging.Presentation/Startup.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MicroBlogging.Configuration;
+using MicroBlogging.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -25,6 +27,12 @@ namespace MicroBlogging
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.Configure<MicroBlogDatabaseSettings>(
+                Configuration.GetSection(nameof(MicroBlogDatabaseSettings)));
+
+            services.AddSingleton<IMicroBlogDatabaseSettings>(sp =>
+                sp.GetRequiredService<IOptions<MicroBlogDatabaseSettings>>().Value);
+            services.AddSingleton<ThreadService>();
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
